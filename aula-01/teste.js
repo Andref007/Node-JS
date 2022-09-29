2342141d0a4d55666cbac8f9a5f9618dd6b98ea8
Andref007 <andrefps007@hotmail.com>
2022-09-29 19:40:49 -0300

    adicao de percentual de memoria usada

--- aula-01/teste.js
+++ aula-01/teste.js
@@ -3,19 +3,28 @@
 const os = require('os')
 
 
+setInterval(() => {
+    const freeMem = parseInt(os.freemem() / 1024 / 1024)
+    const totalMem = parseInt(os.totalmem() / 1024 / 1024)
+    const percent = parseInt((freeMem / totalMem) * 100)
+    const memoria = {
+        total: `${totalMem} MB`,
+        free: `${freeMem} MB`,
+        percent: `${percent} % `
+    }
+
+    console.clear()
+    console.table(memoria)
+}, 1000)
+
+
 //  o módulo OS está dentro da variável OS, desta forma conseguimos acessar as propriedades
 // e métodos desde , míodulo/objeto
-const freeMem = parseInt(os.freemem() / 1024 / 1024) 
-const totalMem = parseInt(os.totalmem() / 1024 / 1024)
 
 // console.log(`Memória livre: ${freeMem}`, ` Memória Total: ${totalMem}`)
 
 
 
-const memoria = {
-    total: `${totalMem} MB`,
-    free: `${freeMem} MB`
-}
 
-console.clear()
-console.table(memoria)
+
+
